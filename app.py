734d603e807fd206d582e42dfe7204e1a08188bc
Шоқан Халхабай <sh.khalkhabay@AD.DG>
2024-12-13 00:40:09 +0500

    new feature

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
+
+# Данные для продуктов
+products_data = {
+    "bearings": [
+        "Роликовые радиальные подшипники для железнодорожного транспорта",
+        "Шариковые подшипники для промышленного оборудования",
+        "Подшипники для сельскохозяйственной техники",
+        "Подшипники для автомобильной промышленности",
+        "Подшипники с угловым контактом для высокоскоростных приложений",
+        "Подшипники для тяжелых условий эксплуатации",
+    ],
+    "asbestos": [
+        "Профилированные листы (шифер)",
+        "Плиты и трубы из асбестоцемента",
+    ]
+}
 
 @app.route('/')
 def home():
@@ -9,10 +25,6 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html')
-
-@app.route('/products')
-def products():
-    return render_template('products.html')
 
 @app.route('/partners')
 def partners():
@@ -30,5 +42,25 @@ def employees():
 def news():
     return render_template('news.html')
 
+@app.route('/products', methods=['GET', 'POST'])
+def products():
+    if request.method == 'POST':
+        action = request.form.get('action')
+        category = request.form.get('category')
+
+        if action == 'add':
+            new_item = request.form.get('new_item')
+            if category and new_item:
+                products_data[category].append(new_item)
+        elif action == 'delete':
+            item_to_delete = request.form.get('item_to_delete')
+            if category and item_to_delete in products_data[category]:
+                products_data[category].remove(item_to_delete)
+
+        return redirect(url_for('products'))
+
+    return render_template('products.html', products_data=products_data)
+
 if __name__ == '__main__':
     app.run(debug=True)
+
